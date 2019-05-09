8dc671503a5c379f401983e37bd4a9f11ba3c88d
Jay Pratap Singh <jpsingh345@gmail.com>
2019-05-09 11:00:24 +0530

    Initial commit: Use camerax to render camera and click pictures

---
+++ app/src/main/java/com/jp/camerax_sample/MainActivity.kt
@@ -0,0 +1,177 @@
+package com.jp.camerax_sample
+
+import android.Manifest
+import android.content.pm.PackageManager
+import android.graphics.Matrix
+import android.os.Bundle
+import android.os.Environment
+import android.util.DisplayMetrics
+import android.util.Log
+import android.util.Rational
+import android.util.Size
+import android.view.Surface
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.camera.core.*
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import androidx.lifecycle.LifecycleOwner
+import kotlinx.android.synthetic.main.activity_main.*
+import java.io.File
+
+class MainActivity : AppCompatActivity(), LifecycleOwner {
+
+    private var lensFacing = CameraX.LensFacing.BACK
+    private val TAG = javaClass::class.java.simpleName
+    private val REQUEST_CAMERA_PERMISSION = 10
+    private val REQUIRED_PERMISSIONS = arrayOf(
+        Manifest.permission.CAMERA,
+        Manifest.permission.WRITE_EXTERNAL_STORAGE
+    )
+    private val folderPath = "${File.separator}CameraX${File.separator}"
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        if (allPermissionsGranted()) {
+            texture.post { startCamera() }
+        } else {
+            ActivityCompat.requestPermissions(
+                this, REQUIRED_PERMISSIONS, REQUEST_CAMERA_PERMISSION
+            )
+        }
+
+        // Every time the provided texture view changes, recompute layout
+        texture.addOnLayoutChangeListener { _, _, _, _, _, _, _, _, _ ->
+            updateTransform()
+        }
+    }
+
+    private fun startCamera() {
+        val metrics = DisplayMetrics().also { texture.display.getRealMetrics(it) }
+        val screenSize = Size(metrics.widthPixels, metrics.heightPixels)
+        val screenAspectRatio = Rational(metrics.widthPixels, metrics.heightPixels)
+        Log.d(javaClass.simpleName, "Metrics: ${metrics.widthPixels} x ${metrics.heightPixels}")
+
+        val previewConfig = PreviewConfig.Builder().apply {
+            setLensFacing(lensFacing)
+            setTargetResolution(screenSize)
+            setTargetAspectRatio(screenAspectRatio)
+            setTargetRotation(windowManager.defaultDisplay.rotation)
+            setTargetRotation(texture.display.rotation)
+        }.build()
+
+        val preview = Preview(previewConfig)
+        preview.setOnPreviewOutputUpdateListener {
+            texture.surfaceTexture = it.surfaceTexture
+            updateTransform()
+        }
+
+
+        // Create configuration object for the image capture use case
+        val imageCaptureConfig = ImageCaptureConfig.Builder()
+            .apply {
+                setLensFacing(lensFacing)
+                setTargetAspectRatio(screenAspectRatio)
+                setTargetRotation(texture.display.rotation)
+                setCaptureMode(ImageCapture.CaptureMode.MAX_QUALITY)
+            }.build()
+
+        // Build the image capture use case and attach button click listener
+        val imageCapture = ImageCapture(imageCaptureConfig)
+        btn_take_picture.setOnClickListener {
+
+            val file = File(
+                Environment.getExternalStorageDirectory().toString() +
+                        "$folderPath${System.currentTimeMillis()}.jpg"
+            )
+
+            imageCapture.takePicture(file,
+                object : ImageCapture.OnImageSavedListener {
+                    override fun onError(
+                        error: ImageCapture.UseCaseError,
+                        message: String, exc: Throwable?
+                    ) {
+                        val msg = "Photo capture failed: $message"
+                        Toast.makeText(baseContext, msg, Toast.LENGTH_SHORT).show()
+                        Log.e(TAG, msg)
+                        exc?.printStackTrace()
+                    }
+
+                    override fun onImageSaved(file: File) {
+                        val msg = "Photo capture succeeded: ${file.absolutePath}"
+                        Toast.makeText(baseContext, msg, Toast.LENGTH_SHORT).show()
+                        Log.d(TAG, msg)
+                    }
+                })
+
+        }
+
+        CameraX.bindToLifecycle(this, preview, imageCapture)
+    }
+
+    private fun updateTransform() {
+        val matrix = Matrix()
+        val centerX = texture.width / 2f
+        val centerY = texture.height / 2f
+
+        val rotationDegrees = when (texture.display.rotation) {
+            Surface.ROTATION_0 -> 0
+            Surface.ROTATION_90 -> 90
+            Surface.ROTATION_180 -> 180
+            Surface.ROTATION_270 -> 270
+            else -> return
+        }
+        matrix.postRotate(-rotationDegrees.toFloat(), centerX, centerY)
+        texture.setTransform(matrix)
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int, permissions: Array<String>, grantResults: IntArray
+    ) {
+        if (requestCode == REQUEST_CAMERA_PERMISSION) {
+            if (allPermissionsGranted()) {
+                createDir()
+                texture.post { startCamera() }
+            } else {
+                Toast.makeText(
+                    this,
+                    "Permissions not granted by the user.",
+                    Toast.LENGTH_SHORT
+                ).show()
+                finish()
+            }
+        }
+    }
+
+
+    private fun allPermissionsGranted(): Boolean {
+        for (permission in REQUIRED_PERMISSIONS) {
+            if (ContextCompat.checkSelfPermission(
+                    this, permission
+                ) != PackageManager.PERMISSION_GRANTED
+            ) {
+                return false
+            }
+        }
+        return true
+    }
+
+
+    private fun createDir() {
+        val dir = File(
+            Environment.getExternalStorageDirectory().toString() +
+                    File.separator + folderPath
+        )
+        try {
+            if (dir.mkdir()) {
+                println("Directory created")
+            } else {
+                println("Directory is not created")
+            }
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+    }
+}
